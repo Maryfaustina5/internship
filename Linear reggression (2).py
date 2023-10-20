914c8a98aaf35760c71b8ed4ffa6d34d0dd825fa
Maryfaustina5 <128983903+Maryfaustina5@users.noreply.github.com>
2023-10-20 21:19:37 +0530

    Assignment completed

---
+++ Linear reggression (2).py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In simple linear regression, the value of what shows the point where the estimated regression line 
+# crosses the 𝑦 axis?
+# a) Y
+# b) B0
+# c) B1
+# d) f
+# 
+# Answer: b) B0
+
+# Check out these four linear regression plots: 
+# 
+# 
+# Which one represents an underfitted model?
+# a)The bottom-left plot
+# b) The top-right plot
+# c) The bottom-right plot
+# d) The top-left plot
+# 
+# Answer: c) The bottom-right plot.
+
+# There are five basic steps when you’re implementing linear regression:
+# • a. Check the results of model fitting to know whether the model is satisfactory.
+# • b. Provide data to work with, and eventually do appropriate transformations.
+# • c. Apply the model for predictions.
+# • d. Import the packages and classes that you need.
+# • e. Create a regression model and fit it with existing data.
+# However, those steps are currently listed in the wrong order. What’s the correct order
+# 
+# Answer: the correct order is: d, b, e, a, c.
+
+# Which of the following are optional parameters to LinearRegression in scikit-learn?
+# a) Fit
+# b) fit_intercept
+# c) normalize
+# d) copy_X
+# e) n_jobs
+# f) reshape
+# 
+# Answer: reshape
+
+# While working with scikit-learn, in which type of regression do you need to transform the array of 
+# inputs to include nonlinear terms such as 𝑥²?
+# a)Multiple linear regression
+# b) Simple linear regression
+# c) Polynomial regression
+# 
+# Answer: c) Polynomial regression
+
+# You should choose statsmodels over scikit-learn when:
+# A)You want graphical representations of your data.
+# b) You’re working with nonlinear terms.
+# c) You need more detailed results.
+# d) You need to include optional parameters
+# 
+# Answer: You need more detailed result 
+
+# _________ is a fundamental package for scientific computing with Python. It offers 
+# comprehensive mathematical functions, random number generators, linear algebra routines, Fourier 
+# transforms, and more. It provides a high-level syntax that makes it accessible and productive.
+# a) Pandas
+# b) Numpy
+# c) Statsmodel
+# d) scipy
+# 
+# Answer: Numpy
+
+#  _________ is a Python data visualization library based on Matplotlib. It provides a high-level 
+# interface for drawing attractive and informative statistical graphics that allow you to explore and 
+# understand your data. It integrates closely with pandas data structures.
+# a) Bokeh
+# b) Seaborn
+# c) Matplotlib
+# d) Das
+# Answer: Seaborn
+
+# Among the following identify the one in which dimensionality reduction reduces.
+# a) Performance
+# b) statistics
+# c) Entropy
+# d) Collinearity
+# 
+# Answer: collinearity
+
+# Which of the following machine learning algorithm is based upon the idea of bagging?
+# a) Decision Tree
+# b) Random Forest
+# c) Classfication
+# d) SVM
+# 
+# Answer: Random Forest
+
+#  Choose a disadvantage of decision trees among the following.
+# a) Decision tree robust to outliers
+# b) Factor analysis
+# c) Decision Tree are prone to overfit
+# d) all of the above
+# 
+# Answer: Decision Tree are prone to overfit
+
+# What is the term known as on which the machine learning algorithms build a model based on 
+# sample data?
+# a) Data Training
+# b) Sample Data
+# c) Training data
+# d) None of the above
+# 
+# Answer: Training data
+
+# Which of the following machine learning techniques helps in detecting the outliers in data?
+# a) Clustering 
+# b) Classification
+# c) Anamoly detection
+# d) All of the above
+# 
+# Answer: anamoly Detection
+
+# Identify the incorrect numerical functions in the various function representation of machine 
+# learning.
+# a) Support Vector 
+# b) Regression
+# c) Case based
+# d) Classification
+# 
+# Answer: case based
+
+# Analysis of ML algorithm needs
+# a) Statistical learning theory
+# b) Computational learning theory
+# c) None of the above
+# d) Both a and b
+# 
+# Answer: Both a and b
+
+# Identify the difficulties with the k-nearest neighbor algorithm.
+# a) Curse of dimensionality
+# b) Calculate the distance of test case for all training cases
+# c) Both a and b
+# d) None
+# 
+# Answer: Both a and b
+
+# The total types of the layer in radial basis function neural networks is ______
+# a) 1
+# b) 2
+# c) 3
+# d) 4
+# 
+# Answer: c
+
+# Which of the following is not a supervised learning
+# a) PCA
+# b) Naïve bayes
+# c) Linear regression
+# d) KMeans
+# 
+# Answer: K Means 
